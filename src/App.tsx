4288e6dcd36682b3b256d75fd7a55777cb08db23
_kegs <kieran_geraghty@hotmail.com>
2024-11-07 07:28:22 +1100

    add config for ts and css modules

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import GameBoard from './components/page/GameBoard/gameBoard';
+
+function App() {
+  return (
+    <div className="App">
+      hello world!
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
