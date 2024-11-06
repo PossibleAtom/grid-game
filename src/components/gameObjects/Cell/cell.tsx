732a948666db588f61f46f1b3008737c7c535267
_kegs <kieran_geraghty@hotmail.com>
2024-11-07 10:49:32 +1100

    added styled components

--- src/components/gameObjects/Cell/cell.tsx
+++ src/components/gameObjects/Cell/cell.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from './cell.module.css'
 import { Placeable } from '../../page/GameBoard/gameBoard';
+import styled from 'styled-components';
 
 interface CellProps {
   selectedPlaceable: Placeable | undefined;
 }
 
+const CellContainer = styled.div<{$contentColor?: string; }>`
+  background-color: ${props => props.$contentColor || '#E9E9E9'}
+`
+
 export const Cell = ({selectedPlaceable}: CellProps) => {
   const [content, setContent] = React.useState(undefined as  Placeable | undefined);
-  return <div onClick={() => setContent(selectedPlaceable)} className={styles.Cell}>{content?.color || ''}</div>
+  return <CellContainer $contentColor={content?.color} onClick={() => setContent(selectedPlaceable)} className={styles.Cell}/>
 }
